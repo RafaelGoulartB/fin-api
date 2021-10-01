146ee6488ee8086780f0a6633f40d8d4a3161cb4
RafaelGoulartB <rafagoulartb@gmail.com>
2021-10-01 08:09:36 -0300

    📦 feat: Add route to search for statement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(express.json())
 
-const customers = [{ id: 1, name: 'John', cpf: '123' }];
+const customers = [{ id: 1, name: 'John', cpf: '123', statement: ['233'] }];
 
 app.post('/account', (request, response) => {
   const { name, cpf } = request.body;
@@ -13,9 +13,19 @@ app.post('/account', (request, response) => {
 
   if(customerAlreadyExist) return response.status(400).send({ error: "Customer already exists!" })
 
-  customers.push({ id: uuidv4(), name, cpf })
+  customers.push({ id: uuidv4(), name, cpf, statement: [] })
 
   return response.sendStatus(201);
+})
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(c => c.cpf === cpf);
+
+  if(!customer) return response.status(404).send({ error: "Customer not found" });
+
+  return response.json(customer.statement)
 })
 
 app.listen(3333)
