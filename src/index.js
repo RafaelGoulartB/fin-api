9cf22e106b4c9bfb2a009c1ce674e3c6b69be21f
RafaelGoulartB <rafagoulartb@gmail.com>
2021-10-03 14:25:09 -0300

    📦 feat: Creating deposit route

--- src/index.js
+++ src/index.js
@@ -36,4 +36,20 @@ app.get('/statement', verifyIfExistAccount, (request, response) => {
   return response.json(customer.statement)
 })
 
+app.post('/deposit', verifyIfExistAccount, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send()
+})
+
 app.listen(3333)
